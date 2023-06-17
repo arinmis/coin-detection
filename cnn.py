4e67c426df1eaf7e849fc46a2caf13a8e8c9d235
arinmis <mustafa_arinmis@outlook.com>
2023-06-17 16:42:06 +0300

    Train CNN

--- cnn.py
+++ cnn.py
@@ -3,6 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 import json
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 
 
 
@@ -37,5 +39,27 @@ val = data.skip(train_size).take(val_size)
 test = data.skip( train_size + val_size).take(test_size)
 
 
+# deep learning model
 
-print(train_size, val_size, test_size)
+model = Sequential()
+
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+
+model.summary()
+
+
+# train
+logdir='logs'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+
