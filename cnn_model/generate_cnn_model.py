0c61231d15d557f4f187ecf2dd3632660f960efa
Seyfullah Sahin OZDEMIR <seyfullahsahinozdemir@gmail.com>
2023-06-18 13:34:51 +0300

    Fix predict

--- cnn_model/generate_cnn_model.py
+++ cnn_model/generate_cnn_model.py
@@ -1,70 +1,148 @@
-import tensorflow as tf
 import numpy as np
-from matplotlib import pyplot as plt
+import cv2
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 import os
-import json
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.models import load_model
+
+# Acquire RGB Coin Image
+def acquire_coin_image(path):
+    image = cv2.imread(path, cv2.IMREAD_COLOR)
+    return image
+
+# Convert RGB Coin Image to Grayscale
+def convert_to_grayscale(image):
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return grayscale_image
+
+# Crop and Trim the Image
+def crop_and_trim_image(image):
+    height, width = image.shape[:2]
+
+    crop_height = min(height, 200)
+    crop_width = min(width, 200)
+
+    start_row = (height - crop_height) // 2
+    end_row = start_row + crop_height
+    start_col = (width - crop_width) // 2
+    end_col = start_col + crop_width
+
+    cropped_image = image[start_row:end_row, start_col:end_col]
+
+    return cropped_image
+
+def crop_circle(image):
+    gray = cv2.medianBlur(image, 5)
+
+    circles = cv2.HoughCircles(
+        gray, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=30, minRadius=0, maxRadius=0
+    )
+
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            x1 = x - r
+            y1 = y - r
+            x2 = x + r
+            y2 = y + r
+            cropped_image = image[y1:y2, x1:x2]
+
+            return cropped_image
+
+    return image
+
+def generate_feature_vector(image):
+    features, _ = hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True)
+    return features
+
+def prepare_images():
+    classes = ['5kr', '10kr', '25kr', '50kr', '1tl']
+
+    current_directory = os.getcwd()
+    image_dir = current_directory + '\cnn_model\data'
+
+    images = []
+    labels = []
+
+    for class_index, class_name in enumerate(classes):
+        class_dir = os.path.join(image_dir, class_name)
+        for filename in os.listdir(class_dir):
+            image_path = os.path.join(class_dir, filename)
+            img = acquire_coin_image(image_path)
+            gray = convert_to_grayscale(img)
+            resized_img = cv2.resize(gray, (416, 416))
+            images.append(resized_img)
+            labels.append(class_index)
+    images = np.array(images)
+    labels = np.array(labels)
+    return (images, labels, classes)
 
 
+def train():
+    (images,labels, classes) = prepare_images()
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
 
-# configs
-data_dir = "data"
+    label_encoder = LabelEncoder()
+    y_train = label_encoder.fit_transform(y_train)
+    y_test = label_encoder.transform(y_test)
 
-with open("coin_desc.json") as f:
-    class_desc = json.load(f)
-print(class_desc)
+    # Etiketleri one-hot kodlamasıyla kodlama
+    num_classes = len(classes)
+    y_train = np_utils.to_categorical(y_train, num_classes)
+    y_test = np_utils.to_categorical(y_test, num_classes)
 
-# data pipleline
-data = tf.keras.utils.image_dataset_from_directory("data", batch_size=24)
-data = data.map(lambda x, y: (x/255, y)) # scale
-data_iterator = data.as_numpy_iterator()
-batch = data_iterator.next()
+    # Build CNN model
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=(416, 416, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation="relu"))
+    model.add(Dense(num_classes, activation="softmax"))
 
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-# images represented as numpy array
-print("len is:", len(batch))
-print(batch[1])
-print("data size", len(data))
+    # Train CNN model
+    model.fit(X_train, y_train, batch_size=32, epochs=20, validation_data=(X_test, y_test))
 
+    # Test CNN model
+    test_loss, test_accuracy = model.evaluate(X_test, y_test)
+    print("Test Loss:", test_loss)
+    print("Test Accuracy:", test_accuracy)
 
-# split data
-train_size = int(len(data) * .7)
-val_size = int(len(data) * .2)
-test_size = int(len(data) * .1)
+    # Save model
+    model.save("coin_detection_model.h5")
+    return (model)
 
+def predict(img, model):
+    new_gray = convert_to_grayscale(img)
+    new_resized = cv2.resize(new_gray, (416, 416))
+    new_input = np.expand_dims(new_resized, axis=0)
+    new_input = np.expand_dims(new_input, axis=-1)
+    cnn_model = model
 
-train = data.take(train_size)
-val = data.skip(train_size).take(val_size)
-test = data.skip( train_size + val_size).take(test_size)
+    (images, labels, classes) = prepare_images()
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+    label_encoder = LabelEncoder()
+    label_encoder.fit_transform(y_train)
+    label_encoder.transform(y_test)
 
-
-# deep learning model
-
-model = Sequential()
-
-model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
-model.add(MaxPooling2D())
-model.add(Conv2D(32, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(16, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
-
-model.summary()
-
-
-# train
-logdir='logs'
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
-hist = model.fit(train, epochs=1, validation_data=val, callbacks=[tensorboard_callback])
-
-# evalutions
-
-# save the model
-model.save(os.path.join('','coin_classifier.h5'))
-
+    # Predict the label
+    predictions = cnn_model.predict(new_input)
+    predicted_class_index = np.argmax(predictions)
+    predicted_class = label_encoder.inverse_transform([predicted_class_index])[0]
+    if predicted_class == 0:
+        return ('5kr', 0.05)
+    elif predicted_class == 1:
+        return ('10kr', 0.1)
+    elif predicted_class == 2:
+        return ('25kr', 0.25)
+    elif predicted_class == 3:
+        return ('50kr', 0.5)
+    else:
+        return ('1tl', 1.0)
--- count_coint.py
+++ count_coint.py
@@ -7,15 +7,17 @@ import contours
 import canny
 import gaussianblur
 import cvt_color as cvt
-
+from keras.models import load_model
+from cnn_model import generate_cnn_model
+import os
 if len(sys.argv) < 2:
     print("usage: python count_coins.py target.jpg")
     exit()
 
-# return numpy array
 image = cv2.imread(sys.argv[1])
+#image = cv2.imread(os.getcwd()+"\\tests\\test_test.png")
 gray = cvt.rgb2gray(image)
-plt.imshow(gray, cmap='gray');
+plt.imshow(gray, cmap='gray')
 
 # 1. make it blur
 blur = gaussianblur.gaussian_blur(gray, 31, 1)
@@ -27,22 +29,36 @@ canny = canny.canny_edge_detection(blur)
 plt.imshow(canny, cmap='gray')
 
 # make edges clear
-dilated = dilate.dilate_image(canny, (4, 4), iterations = 5)
+dilated = dilate.dilate_image(canny, (3, 3), iterations = 2)
 plt.imshow(dilated, cmap='gray')
 plt.show()
 
 # 3. Contour the coins
 (cnt, heirarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-cv2.drawContours(rgb, cnt, -1, (255, 0,0), 2) #  
+cv2.drawContours(rgb, cnt, -1, (255, 0,0), 2)
 
-# cnt= contours.find_contours(dilated.copy())
-# cnt= cv2.find_contours(dilated.copy())
-# rgb = cvt.bgr2rgb(image)
-# contours.draw_contours(rgb, cnt,(255, 0,0))  
+
 
 # put model here
-# https://blog.paperspace.com/writing-cnns-from-scratch-in-pytorch/
-print('Coins in the image: ', len(cnt))
-plt.imshow(rgb)
-plt.show()
+cnn_model = load_model("cnn_model/coin_detection_model.h5")
+
+# Array to store pixel values of each circle
+circle_pixels = []
+# List to store cropped coin images
+coin_images = []
+# Iterate over each contour and crop the coins
+result = 0
+for contour in cnt:
+    (x, y, w, h) = cv2.boundingRect(contour)
+    coin = image[y:y+h, x:x+w]
+    coin_images.append(coin)
+
+for coin in coin_images:
+  predicted_label, amount = generate_cnn_model.predict(coin, cnn_model)
+  result += amount
+  plt.imshow(coin)
+  plt.title(predicted_label)
+  plt.show()
+
+print("Total amount: " + str(result))
